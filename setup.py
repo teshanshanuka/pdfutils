abc818ddbd40ffd42233754cf4dc47e1a85b8705
Teshan Liyanage <teshanuka@gmail.com>
2024-03-23 11:36:59 +0100

    chore: add requirements

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="pdfutils",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
+    install_requires=requirements,
     entry_points={
         "console_scripts": [
             "pdfutils = pdfutils.main:main",
